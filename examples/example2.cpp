199cbf4b2899377df4218461c3a59b233a526ded
Antoine Viallon <antoine.viallon@gmail.com>
2017-12-20 21:51:56 +0100

    Removed a useless commented line in example2.cpp

--- examples/example2.cpp
+++ examples/example2.cpp
@@ -3,13 +3,15 @@
 void mouseMove(Allegro* allegro, void* context, unsigned char event, int x, int y){
 	allegro->draw_rectangle(0, 0, 640, 480, allegro->rgb(255, 255, 255), 1, true); // we erase the screen
 	allegro->draw_ellipse_r(x, y, 8, 8, allegro->rgb(0, 0, 0), 2, false); //... and draw an circle at the mouse coordinates.
+
+	// The context pointer is used to share any kind of information between different functions bound to events. I'll make an example about that later.
 }
 
 int main(){
 	Allegro* allegro = new Allegro();
 	allegro->init(); // Required before doing anything else
 	allegro->createWindow(30, 640, 480); // Create a window with a width of 640 and a height of 480, refreshing at 30 fps.
-	//allegro->draw_text(640/2, 480/2, "Hello, world!", allegro->rgb(0, 0, 0)); // Draw "Hello, world!" in the middle of the screen (in black).
+
 
 	allegro->bindMouseMove(mouseMove); // bind mouse moved events to mouseMove function
 	allegro->gameLoop(); // starts the loop
