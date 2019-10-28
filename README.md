6b3ffd2e1564084cbd788b9d69df0b0479676026
Antoine Viallon <antoine.viallon@gmail.com>
2019-10-28 02:36:52 +0100

    Fixed README.md again, added example1 (and fixed it)

--- README.md
+++ README.md
@@ -13,13 +13,20 @@ you instantiate an `Allegro` object and everything uses it.
 ```c++
 #include <allegro++/allegro.h>
 
+using namespace AllegroPP;
+
+void draw(Allegro* allegro, float fps){
+   allegro->clearScreen(); // screen is not cleared by default
+   allegro->draw_text(640/2, 480/2, "Hello, world!", allegro->rgb(0, 0, 0)); // Draw "Hello, world!" in the middle of the screen (in black).
+}
+
 int main(){
    Allegro::init(); // Required before doing anything else
    Allegro window = Allegro();
 
-   window.createWindow(30, 640, 480); // Create a window with a width of 640 and a height of 480, refreshing at 30 fps.
+   window.createWindow(30, 640, 480, ALLEGRO_WINDOWED); // Create a window with a width of 640 and a height of 480, refreshing at 30 fps.
 
-   window.draw_text(640/2, 480/2, "Hello, world!", allegro->rgb(0, 0, 0)); // Draw "Hello, world!" in the middle of the screen (in black).
+   window.setRedrawFunction(&draw);
 
    Allegro::startLoop();
 
--- examples/example1.cpp
+++ examples/example1.cpp
@@ -1,15 +1,21 @@
 #include <allegro++/allegro.h>
 
+using namespace AllegroPP;
+
+void draw(Allegro* allegro, float fps){
+   allegro->clearScreen(); // screen is not cleared by default
+   allegro->draw_text(640/2, 480/2, "Hello, world!", allegro->rgb(0, 0, 0)); // Draw "Hello, world!" in the middle of the screen (in black).
+}
+
 int main(){
- 	Allegro* allegro = new Allegro();
-	allegro->init(); // Required before doing anything else
-	allegro->createWindow(30, 640, 480); // Create a window with a width of 640 and a height of 480, refreshing at 30 fps.
+   Allegro::init(); // Required before doing anything else
+   Allegro window = Allegro();
 
-	allegro->draw_text(640/2, 480/2, "Hello, world!", allegro->rgb(0, 0, 0)); // Draw "Hello, world!" in the middle of the screen (in black).
+   window.createWindow(30, 640, 480, ALLEGRO_WINDOWED); // Create a window with a width of 640 and a height of 480, refreshing at 30 fps.
 
-	allegro->gameLoop(); // starts the loop
+   window.setRedrawFunction(&draw);
 
-	delete allegro;
+   Allegro::startLoop();
 
-	return 0;
+   return 0;
 }
